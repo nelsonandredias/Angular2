d81c7fb2f11f49b3d8267f4e3daee5a5664c06b5
nelson <srandrediaz@gmail.com>
2019-08-05 17:51:46 +0100

    created recipe-app: built the basic structure

---
+++ recpite-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    // tslint:disable-next-line: max-line-length
+    new Recipe('A test recipe 1', 'This is simple a test', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'),
+    // tslint:disable-next-line: max-line-length
+    new Recipe('A test recipe 2', 'This is simple a test', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'),
+    // tslint:disable-next-line: max-line-length
+    new Recipe('A test recipe 3', 'This is simple a test', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/sucess-alert/sucess-alert.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sucess-alert',
-  templateUrl: './sucess-alert.component.html',
-  styleUrls: ['./sucess-alert.component.css']
-})
-export class SucessAlertComponent {
-}
