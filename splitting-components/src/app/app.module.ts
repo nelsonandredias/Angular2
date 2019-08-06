a2afd0809bd5242edf68909ae9bb093b7294b41e
nelson <srandrediaz@gmail.com>
2019-08-06 14:28:52 +0100

    added basic app structure

--- splitting-components/src/app/app.module.ts
+++ splitting-components/src/app/app.module.ts
@@ -2,28 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SucessAlertComponent } from './sucess-alert/sucess-alert.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SucessAlertComponent,
     CockpitComponent,
     ServerElementComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
