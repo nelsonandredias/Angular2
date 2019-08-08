693dc83151c472e20bd61ba7133b7adcbe30995c
nelson <srandrediaz@gmail.com>
2019-08-08 12:18:44 +0100

    added custom directive to dropdown menus

---
+++ recpite-app/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  @HostBinding('class.open') isOpen = false;
+
+  constructor() { }
+
+  @HostListener('click') click() {
+    this.isOpen = !this.isOpen;
+  }
+
+  @HostListener('focusout') focusout() {
+    this.isOpen = false;
+  }
+
+}
