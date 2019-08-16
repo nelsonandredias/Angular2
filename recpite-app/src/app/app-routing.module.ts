f86825ba9d2240f0b581c209e5bef4a3f28d3330
nelson <srandrediaz@gmail.com>
2019-08-16 11:08:31 +0100

    added new child path to recipes

--- recpite-app/src/app/app-routing.module.ts
+++ recpite-app/src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 const appRoutes: Routes = [
-  {path: 'recipes' , component: RecipesComponent},
+  {path: 'recipes' , component: RecipesComponent, children: [
+    {path: '', component: RecipesStartComponent },
+    {path: ':id', component: RecipeDetailComponent }
+  ]},
   {path: 'shopping-list', component: ShoppingListComponent},
   {path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ]
