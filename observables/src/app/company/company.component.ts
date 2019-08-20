d3e40ab33a79343607a546082800b23b96db0721
nelson <srandrediaz@gmail.com>
2019-08-20 14:48:58 +0100

    building a custom observable

--- observables/src/app/company/company.component.ts
+++ observables/src/app/company/company.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
   styleUrls: ['./company.component.css']
 })
-export class CompanyComponent implements OnInit {
+export class CompanyComponent implements OnInit, OnDestroy {
+
+  private firstOnSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+
+    // create a new custom observable
+    const customIntervalObservable = Observable.create(
+      observer => {
+        let count = 0;
+        setInterval( () => {
+          observer.next(count);
+          count++;
+        } , 1000);
+      }
+    );
+
+    // subscribe to custom observable
+    this.firstOnSubscription = customIntervalObservable.subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+
+  }
+
+  ngOnDestroy() {
+    // unsubscribe custom observable
+    this.firstOnSubscription.unsubscribe();
+
   }
 
 }
