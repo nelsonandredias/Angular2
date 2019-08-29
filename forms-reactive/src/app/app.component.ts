94d652d931d13d89f80a03f447e320e44918dc9a
nelson <srandrediaz@gmail.com>
2019-08-29 11:37:32 +0100

    add a custom validator to a FormControl

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -10,6 +10,9 @@ export class AppComponent implements OnInit {
 
   genders = ['male', 'female'];
 
+  // create usernames for custom validators
+  forbiddenUsernames = ['Ana', 'Maria', 'Joao'];
+
   // property that will hold the form
   signupForm: FormGroup;
 
@@ -18,7 +21,7 @@ export class AppComponent implements OnInit {
     // create and initialize a reactive form
     this.signupForm = new FormGroup({
 
-      usernameReactive: new FormControl(null, Validators.required),
+      usernameReactive: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
       emailReactive: new FormControl(null, [Validators.required, Validators.email]),
       contactData: new FormGroup({
         addressReactive: new FormControl(null, Validators.required),
@@ -31,16 +34,27 @@ export class AppComponent implements OnInit {
   }
 
   onAddHobby() {
+    // create a new FormControl template
     const newFormControl = new FormControl(null, Validators.required);
+    // add the new FormControl to the formArray
     (this.signupForm.get('hobbiesReactive') as FormArray).push(newFormControl);
   }
 
 
   onSubmit() {
-
     // submit the form based on the reactive form
     console.log(this.signupForm);
+  }
 
+  // create a custom name validator for the userNameReactive formControl
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+
+    // verify typed name is forbidden (if exists in the the array of forbiddenUsernames)
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    // otherwise, return null because it's valid
+    return null;
   }
 
 }
